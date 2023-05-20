b25cbc05a1f24b8b7fbf234016298044c5717c5e
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-05-20 22:45:53 +0600

    get toys from db and vercel deploy

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require("express");
-require("dotenv").config();
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.j1jxfgc.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri);
+
+app.use(cors());
+app.use(express.json());
 
 async function run() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(`You successfully connected to MongoDB!`);
+    const toysCollection = client.db("toysDB").collection("toys");
+
+    app.get("/toys", async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
   }
